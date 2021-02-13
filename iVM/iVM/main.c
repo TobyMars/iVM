fc6f9946dff790fda4aa37f25b0a29954001f0f8
TobyM <hk.mars@aol.com>
2021-02-13 17:07:26 +0800

    Create macOS project for iVM

---
+++ iVM/iVM/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  iVM
+//
+//  Created by TobyMars on 2021/2/13.
+//  Copyright © 2021 TobyMars. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
